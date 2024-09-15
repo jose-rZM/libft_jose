a85e18d3c9190fbc915a986f8654e710d2507907
Jose <jramirezmorad@gmail.com>
2024-09-15 20:20:47 +0200

    modify ft_atoi to match GNUlibc impl

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,31 +6,49 @@
 /*   By: jramire2 <jramire2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 13:03:37 by jramire2          #+#    #+#             */
-/*   Updated: 2024/09/11 13:05:40 by jramire2         ###   ########.fr       */
+/*   Updated: 2024/09/15 20:14:31 by jramire2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+
+static int	ft_isspace(int c)
+{
+	return (c == 32 || (c >= 9 && c <= 13));
+}
+
+static long ft_decimal_strtol(const char *nptr)
+{
+    long    ret;
+    int sign;
+
+    ret = 0;
+    sign = 1;
+    while (ft_isspace(*nptr))
+        ++nptr;
+    if (*nptr == '-' || *nptr == '+')
+    {
+        if (*nptr == '-')
+            sign = -1;
+        ++nptr;
+    }
+    while (ft_isdigit(*nptr))
+    {
+        if (ret > (LONG_MAX / 10))
+        {
+            if (sign == 1)
+                return (LONG_MAX);
+            else
+                return (LONG_MIN);
+        }
+        ret = (ret * 10) + (*nptr - '0');
+        ++nptr;
+    }
+    return (ret * sign);
+}
 
 int	ft_atoi(const char *nptr)
 {
-	int	ret;
-	int	sign;
-
-	ret = 0;
-	sign = 1;
-	if (*nptr == '-' || *nptr == '+')
-	{
-		if (*nptr == '-')
-		{
-			sign = -1;
-		}
-		++nptr;
-	}
-	while (ft_isdigit(*nptr))
-	{
-		ret = (ret * 10) + (*nptr - '0');
-		++nptr;
-	}
-	return (ret * sign);
+    return ((int) ft_decimal_strtol(nptr));
 }
